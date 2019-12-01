bf90628020ba4297ae9b5e6c3f0b90a781224ac3
kiranbhalerao123 <bhaleraok65@gmail.com>
2019-12-01 22:49:19 +0530

    remove Bearer from token and rebuild

--- src/Http.ts
+++ src/Http.ts
@@ -3,46 +3,47 @@ import axios, {
   AxiosRequestConfig,
   AxiosResponse,
   Method
-} from 'axios'
+} from "axios";
 
 export default class Http {
   constructor(private baseUrl: string) {}
 
   public get = (url: string, token: string | null, data?: any, options?: any) =>
-    this.fetch(url, token, options)
+    this.fetch(url, token, options);
 
   public post = (
     url: string,
     token: string | null,
     data: any,
     options?: AxiosRequestConfig
-  ) => this.fetchWithBody('POST', url, token, data, options)
+  ) => this.fetchWithBody("POST", url, token, data, options);
 
   public delete = (
     url: string,
     token: string | null,
     data: any,
     options?: AxiosRequestConfig
-  ) => this.fetchWithBody('DELETE', url, token, data, options)
+  ) => this.fetchWithBody("DELETE", url, token, data, options);
 
   public put = (
     url: string,
     token: string | null,
     data: any,
     options?: AxiosRequestConfig
-  ) => this.fetchWithBody('PUT', url, token, data, options)
+  ) => this.fetchWithBody("PUT", url, token, data, options);
 
-  private absUrl = (relativePath: string): string => this.baseUrl + relativePath
+  private absUrl = (relativePath: string): string =>
+    this.baseUrl + relativePath;
 
   private getHeaders = (token: string | undefined | null) => {
     if (token)
       return {
-        Authorization: `Bearer ${token}`,
-        'Access-Control-Allow-Origin': '*'
-      }
+        Authorization: token,
+        "Access-Control-Allow-Origin": "*"
+      };
 
-    return {}
-  }
+    return {};
+  };
 
   private fetch = (
     url: string,
@@ -56,9 +57,9 @@ export default class Http {
       })
       .then((res: AxiosResponse) => res)
       .catch((err: AxiosError) => {
-        throw err
-      })
-  }
+        throw err;
+      });
+  };
 
   private fetchWithBody = (
     method: Method,
@@ -76,7 +77,7 @@ export default class Http {
     })
       .then((res: AxiosResponse) => res)
       .catch((err: AxiosError) => {
-        throw err
-      })
-  }
+        throw err;
+      });
+  };
 }
