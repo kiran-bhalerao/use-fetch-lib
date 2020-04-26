ba4b4a926ebd4734baf5e8c9b46218a8a838994f
kiranbhalerao1234 <kiran@vrlabtech.com>
2020-04-26 17:05:47 +0530

    build with terser to minify code

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,19 @@
+import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 
 export default [
   {
     input: "index.ts",
-    external: Object.keys(pkg.peerDependencies || {}),
+    external: Object.keys(pkg.dependencies || {}).concat(
+      Object.keys(pkg.peerDependencies || {})
+    ),
     plugins: [
       typescript({
-        typescript: require("typescript")
-      })
+        typescript: require("typescript"),
+      }),
+      terser(),
     ],
-    output: [
-      { file: pkg.main, format: "cjs" }
-    ]
-  }
+    output: [{ file: pkg.main, format: "cjs" }],
+  },
 ];
